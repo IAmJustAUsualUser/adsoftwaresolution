9631f78ab7a523e37ca5c14b3c510fcf4a27a936
Anup <anupdharel1@gmail.com>
2024-07-03 19:58:18 +0545

    fix navbar, edit testimonials, add about us page

--- js/script.js
+++ js/script.js
@@ -1,21 +1,16 @@
-function Calculate() {
-  const P = document.getElementById("Principle").valueAsNumber;
-  const T = document.getElementById("Time").valueAsNumber;
-  const R = document.getElementById("Rate").valueAsNumber;
-  return (document.getElementById("SI").innerHTML = (P * T * R) / 100);
-}
+// change Heading(h2) text
+// const Heading2 = document.querySelector("h2");
+// Heading2.addEventListener("click", function () {
+//   Heading2.innerHTML = "Text Changed";
+// });
 
-const Heading2 = document.querySelector("h2");
-Heading2.addEventListener("click", function () {
-  Heading2.innerHTML = "Text Changed";
-});
-
-const Heading1 = document.querySelector("h1");
-Heading1.addEventListener("click", function () {
-  Heading1.style.fontSize = "49px";
-  Heading1.style.color = "Green";
-  Heading1.style.textAlign = "Center";
-});
+// change h1 (font color and align)
+// const Heading1 = document.querySelector("h1");
+// Heading1.addEventListener("click", function () {
+//   Heading1.style.fontSize = "49px";
+//   Heading1.style.color = "Green";
+//   Heading1.style.textAlign = "Center";
+// });
 
 // Go to Top
 const gotop = document.querySelector("#gototop");
